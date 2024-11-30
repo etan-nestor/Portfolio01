ff725a255f934bb5a1544955c9dddd01eb2d7897
dev <dev@dev>
2024-11-30 13:03:56 +0000

    commit03

--- Client/Pages/Home/index.js
+++ Client/Pages/Home/index.js
@@ -24,4 +24,16 @@ button.addEventListener("click", function () {
   }
 });
 
+// mobile menu
 
+// navbar variables
+const navbarNav = document.querySelector('.navbar-nav');
+const navbarToggleBtn = document.querySelector('.nav-toggle-btn');
+
+// navbar toggle functionality
+navbarToggleBtn.addEventListener('click', function () {
+
+  navbarNav.classList.toggle('active');
+  this.classList.toggle('active');
+
+});
